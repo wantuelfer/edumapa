f044ac56e4d29cc1ebf1629adefa72e48363e91e
EduMapa <edumapa@protonmail.com>
2024-11-26 10:31:12 -0300

    Update paginas.js

--- js/paginas.js
+++ js/paginas.js
@@ -186,8 +186,6 @@ function escrevelista(){
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(map);
 
-  markers = L.markerClusterGroup();
-
   escrevelista2();
 }
 
